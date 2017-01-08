e31f9db622c9b48c78a2fa2f01694a7719f1beba
roarwrecker <helmut.rohregger@gmail.com>
2017-01-08 14:56:06 +0100

    Minor changes in TestHelper.ps1

--- roarwrecker.testing/tests/TestHelper.ps1
+++ roarwrecker.testing/tests/TestHelper.ps1
@@ -1,27 +1,21 @@
 function New-TestModule
 {
-    Param([string]$Name)
+    Param([string] $Name)
 
     # The actual module folder
-    $moduleFolder = New-Item -Path "$TestDrive\$Name" -ItemType Directory
+    $moduleFolder = New-Item -Path "${TestDrive}\${Name}" -ItemType Directory
     # The corresponding module.psm1 file
-    New-Item -Path "$moduleFolder\$Name.psm1" -ItemType File | Out-Null
+    New-Item -Path "${moduleFolder}\${Name}.psm1" -ItemType File | Out-Null
 
     Write-Output $moduleFolder
 }
 
 function New-TestScript
 {
-    Param(
-        [string]
-        $Path,
-
-        [string]
-        $Content
-    )
+    Param([string] $Path, [string] $Content)
 
     $scriptPath = New-Item -Path $Path -ItemType File -Force
     Set-Content -Path $scriptPath -Value $Content
 
-    $scriptPath
+    Write-Output $scriptPath
 }
